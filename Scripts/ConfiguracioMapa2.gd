7ab3a5584f1bf7a514f2c06c658d8cecb44d3e4a
Teo Clerici <75330678+teo-clerk@users.noreply.github.com>
2025-05-03 18:38:31 +0200

    done

--- Scripts/ConfiguracioMapa2.gd
+++ Scripts/ConfiguracioMapa2.gd
@@ -4,7 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.escena_anerior = "res://Scenes/mapa_2.tscn"# Replace with function body.
-	Global.claus_necessaries = 7
+	Global.claus_necessaries = 3
 	Global.claus = 0
 	Global.monedes = 0
-	Global.pollastres_necessaris = 92
+	Global.pollastres_necessaris = 0
